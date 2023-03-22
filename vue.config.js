1619cc803af49cb6f084eebc29449fefa5ca1ab8
bkrewer <brnkrewer@gmail.com>
2023-03-22 20:48:08 -0300

    feat: remove vue compat

--- vue.config.js
+++ vue.config.js
@@ -4,23 +4,6 @@ module.exports = {
   chainWebpack(config) {
     config.resolve.alias
       .set('@', path.join(__dirname, './src'))
-
-    config.resolve.alias.set('vue', '@vue/compat')
-
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap((options) => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 3
-            }
-          }
-        }
-      })
-
   },
   css: {
     loaderOptions: {
